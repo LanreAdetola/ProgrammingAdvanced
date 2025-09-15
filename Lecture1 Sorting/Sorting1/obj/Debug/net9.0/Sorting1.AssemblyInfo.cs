0791fdc0faa1287d30b2e7643bc38b85387acd7e
LanreAdetola <100709089+LanreAdetola@users.noreply.github.com>
2025-09-15 15:51:42 +0200

    Update assembly informational version and cache inputs

--- Lecture1 Sorting/Sorting1/obj/Debug/net9.0/Sorting1.AssemblyInfo.cs
+++ Lecture1 Sorting/Sorting1/obj/Debug/net9.0/Sorting1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sorting1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa7a493101598fa120adbe22053353ace63e019b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a630cde1a49e29b4747d441170b81dab5410374d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sorting1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sorting1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
