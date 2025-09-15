fa7a493101598fa120adbe22053353ace63e019b
LanreAdetola <100709089+LanreAdetola@users.noreply.github.com>
2025-09-15 15:11:47 +0200

    Add number guessing game implementation and sorted numbers file

--- Lecture1 Sorting/Sorting1/Program.cs
+++ Lecture1 Sorting/Sorting1/Program.cs
@@ -1,1 +1,30 @@
 ﻿
+using System.Runtime;
+
+string[] lines = File.ReadAllLines("sorted_numbers.txt");
+
+// foreach (string line in lines)
+//  {
+//    Console.WriteLine(line);
+//  }
+// To read numbers from a file and store them in an array
+
+Random rand = new Random();
+
+int target = rand.Next(Convert.ToInt32(lines[0]), Convert.ToInt32(lines[lines.Length - 1]) + 1);
+
+Console.WriteLine("Targeted number: " + target);
+
+int guess = rand.Next(Convert.ToInt32(lines[0]), Convert.ToInt32(lines[lines.Length - 1]) + 1);
+
+int guessNumber = 1;
+
+while (guess != target)
+{
+    guess = rand.Next(Convert.ToInt32(lines[0]), Convert.ToInt32(lines[lines.Length - 1]) + 1);
+
+    guessNumber++;
+
+}
+
+Console.WriteLine("Found the number " + target + " in " + guessNumber + " guesses.");
