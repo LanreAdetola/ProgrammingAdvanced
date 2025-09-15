a630cde1a49e29b4747d441170b81dab5410374d
LanreAdetola <100709089+LanreAdetola@users.noreply.github.com>
2025-09-15 15:50:06 +0200

    Implement linear and binary search algorithms with updated output messages

--- Lecture1 Sorting/Sorting1/Program.cs
+++ Lecture1 Sorting/Sorting1/Program.cs
@@ -1,6 +1,4 @@
 ﻿
-using System.Runtime;
-
 string[] lines = File.ReadAllLines("sorted_numbers.txt");
 
 // foreach (string line in lines)
@@ -27,4 +25,42 @@ while (guess != target)
 
 }
 
-Console.WriteLine("Found the number " + target + " in " + guessNumber + " guesses.");
+Console.WriteLine("Stupid search: Number was " + target + ", guessed in " + guessNumber + " guesses.");
+
+
+//Linear Search
+int count = 0;
+
+for (int i = 0; i < lines.Length; i++)
+{
+    count = count + 1;
+    if (Convert.ToInt32(lines[i]) == target)
+    {
+        Console.WriteLine("Linear search: Number was " + guess + ", guessed in " + count + " guesses!");
+        break;
+    }
+}
+
+
+//Binary Search
+int low = 0;
+int high = lines.Length - 1;
+int counter = 1;
+
+int middle = (low + high) / 2;
+
+while (Convert.ToInt32(lines[middle]) != target)
+{
+    counter = counter + 1;
+    if (Convert.ToInt32(lines[middle]) < target)
+    {
+        low = middle + 1;
+    }
+    else
+    {
+        high = middle - 1;
+    }
+    middle = (low + high) / 2;
+    count = count + 1;
+}
+Console.WriteLine("Binary search: Number was " + guess + ", guessed in " + counter + " guesses!");
--- Lecture1 Sorting/Sorting1/obj/Debug/net9.0/Sorting1.AssemblyInfo.cs
+++ Lecture1 Sorting/Sorting1/obj/Debug/net9.0/Sorting1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sorting1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29e9908a5c240a2eae3f59d6cf295ff0076ec81f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa7a493101598fa120adbe22053353ace63e019b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sorting1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sorting1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
